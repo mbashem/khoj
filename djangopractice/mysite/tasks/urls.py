70e6340ffd671449befce896a37794de57bae109
Farhan Ishraq <omifarhan@gmail.com>
2021-10-20 01:07:39 +0600

    created a text util website with some basic features

---
+++ djangopractice/mysite/tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .import views
+
+urlpatterns = [
+
+    path('', views.index),
+    path('profile', views.profile),
+    path('file', views.file),
+    path('checkcommand', views.Check_Command),
+
+]
