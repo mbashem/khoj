f9a820533d50b534c6e93dd509fb98b69064e184
MaishaAmin <maisha.amin@northsouth.edu>
2021-10-21 22:25:51 +0600

    Django Personal Navigator

--- DjangoPractice/practice/practice/urls.py
+++ DjangoPractice/practice/practice/urls.py
@@ -1,4 +1,4 @@
-"""Test URL Configuration
+"""practice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .  import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path
+     path('', views.link, name= 'link'),
+    path('git', views.git, name= 'git')
+
 ]
