42de58846da6b6049fb84fac5dd1a3346340c00a
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-10-24 00:08:38 +0600

    pylucene

---
+++ practice/pylucene-8.9.0-src/pylucene-8.9.0/jcc/build/lib.win-amd64-3.10/jcc/config.py
@@ -0,0 +1,8 @@
+
+INCLUDES=['C:\\Program Files\\Java\\jdk1.8.0_311/include', 'C:\\Program Files\\Java\\jdk1.8.0_311/include/win32']
+CFLAGS=['/EHsc', '/D_CRT_SECURE_NO_WARNINGS']
+DEBUG_CFLAGS=['/Od', '/DDEBUG']
+LFLAGS=['/DLL', '/LIBPATH:C:\\Program Files\\Java\\jdk1.8.0_311/lib', 'Ws2_32.lib', 'jvm.lib']
+IMPLIB_LFLAGS=['/IMPLIB:%s']
+SHARED=True
+VERSION="3.10"
