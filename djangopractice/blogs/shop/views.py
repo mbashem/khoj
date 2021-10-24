670b0851f60b695651af7b4ee0716dc74b154ff3
Farhan Ishraq <omifarhan@gmail.com>
2021-10-24 21:06:46 +0600

    practice templating and learning templating syntax

--- djangopractice/blogs/shop/views.py
+++ djangopractice/blogs/shop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import myblogs
 
 from django.http import  HttpResponse
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+    blogs = myblogs.objects.all()
+    print(blogs)
+    slides = len(blogs)
+    params = {'blogs': blogs, 'range': range(slides), 'slides':slides}
+    return render(request, 'shop/index.html', params)
+
+def about(request):
+    return render(request, 'shop/about.html')
