3d684cec42a65233f2dd27f80358e35a4bff0480
Farhan Ishraq <omifarhan@gmail.com>
2021-10-28 00:04:09 +0600

    install poetry and add virtual env

--- practice-omi/blogs/blogs/views.py
+++ practice-omi/blogs/blogs/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def index(request):
-    return HttpResponse("This is main home page")
+    return render(request, 'login_register.html')
