092b73067ee18525a0504c9a750962d68ebbebc7
Farhan Ishraq <omifarhan@gmail.com>
2021-10-31 11:43:26 +0600

    Stored scraped data in jason file

--- practice-omi/django_poetry_example/turotial/turotial/items.py
+++ practice-omi/django_poetry_example/turotial/turotial/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class TurotialItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+
--- practice-omi/django_poetry_example/turotial/turotial/spiders/quotes_spider.py
+++ practice-omi/django_poetry_example/turotial/turotial/spiders/quotes_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from ..items import TurotialItem
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -7,14 +7,18 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+
+        items = TurotialItem()
+
         all_div_quotes = response.css('div.quote')
 
         for quotes in all_div_quotes:
             title = quotes.css('span.text::text').extract()
             author = quotes.css('.author::text').extract()
             tags = quotes.css('.tag::text').extract()
-            yield {
-                'title' : title,
-                'author' : author,
-                'tags' : tags
-            }
+
+            items['title'] = title
+            items['author'] = author
+            items['tags'] = tags
+
+            yield items
