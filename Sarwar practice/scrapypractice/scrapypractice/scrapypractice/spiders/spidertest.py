8d25ba5d0284889c843f6a93a9628123ee62c510
Sarwar Khalid <56037779+Nekrodeath@users.noreply.github.com>
2021-11-06 14:42:09 +0600

    Scrapy BFS practice

--- Sarwar practice/scrapypractice/scrapypractice/scrapypractice/spiders/spidertest.py
+++ Sarwar practice/scrapypractice/scrapypractice/scrapypractice/spiders/spidertest.py
@@ -1,32 +1,62 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
 class testspider(scrapy.Spider):
+
     name = "testspider"
+   
+    DEPTH_PRIORITY = 1
+    SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+    SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        
-
+            'https://quotes.toscrape.com/page/1/'
     ]
+    
 
-   
     def start_requests(self):
-        return [scrapy.Request('http://quotes.toscrape.com/page/1/',callback=self.parse, cb_kwargs=dict(cnt = 2))]
+        return [scrapy.Request('http://localhost/Crawl_check/p1.html',callback=self.parse, cb_kwargs=dict(cnt = 1))]
 
          
 
     def parse(self,response,cnt):
 
+        
+        yield {'url' : response.url,'depth' : cnt}
+        print('THE CURRENT URL IS '+response.url)
+
+        for txt in response.css("::text"):
+            var = txt.get().strip()
+            if len(var) != 0:
+                yield {'text' : var}
          
-        yield{'url' : response.url,'depth' : cnt}
-       
-        yield scrapy.Request('http://quotes.toscrape.com/page/2/',callback=self.parse, cb_kwargs=dict(cnt = cnt+1))
-        
-        
-                
-        
+
+
+        if(cnt<10):
+
+             for nextpage in response.css('a::attr(href)'):
+                 nextpage = nextpage.get()
+                 if nextpage is not None:
+                     print('THE URL IS '+response.urljoin(nextpage))
+                     yield scrapy.Request(response.urljoin(nextpage),callback=self.parse, cb_kwargs=dict(cnt = cnt+1))  
+                     
+
+
+process = CrawlerProcess(settings={
+    "FEEDS": {
+        "scrapped.json": {"format": "json"},
+    },
+})
+
+
+process.crawl(testspider)
+print("Starting crawl")
+process.start()
+print("Crawl ended")
+                    
+
+
             
 
 
-        
