26defab703fc9513a8f75c8771c425677130d874
Farhan Ishraq <omifarhan@gmail.com>
2021-11-24 15:34:02 +0600

    edit in template

--- website/CreateClusters/urls.py
+++ website/CreateClusters/urls.py
@@ -1,21 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
-<<<<<<< HEAD
+
 
 from . import views
 from django.urls import path,include
-=======
+
 from . import views
 from django.urls import path,include
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
->>>>>>> Maisha
 urlpatterns = [
 
     path('', views.index),
 
 ]
-<<<<<<< HEAD
-=======
-# urlpatterns += staticfiles_urlpatterns()
 
->>>>>>> Maisha
