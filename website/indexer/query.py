c30dcdeaf80e6dedf47405bffb7a85b0636d074c
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-11-25 00:56:32 +0600

    solr query and update

--- website/indexer/query.py
+++ website/indexer/query.py
@@ -1,30 +1,45 @@
 from typing import List
 import requests
+import os
+from dotenv import load_dotenv
 
-def query_solr(key: str, mx_depth: int, data_type: str, sites: List[str]):
+load_dotenv()
+
+def query_solr(
+        key: str,
+        mx_depth: int,
+        data_type: str,
+        sites: List[str],
+        start: int = 0,
+        row: int = 10):
     """
     Queries solr and returns query result
     """
-    
-    cluster = "save_cluster"
+
+    solr_url = os.getenv("SOLR_URL")
     sites_list = " or ".join(sites)
 
-    query = ""
     sites_list = "url:(" + sites_list + ")"
     depth_query = "depth:[ * TO " + str(mx_depth) + " ]"
     data_type = "data_type:"+data_type
 
-    payload = "fq=" + key + "&fq=" + sites_list + \
-        "&fq=" + depth_query + "&fq=" + data_type
+    payload = f"q=text:{key}~&fq={sites_list}&fq={depth_query}&fq={data_type}"
 
-    print(sites_list)
-
-    print(payload)
-    url = "http://localhost:8983/solr/{cluster}/select?{payload}&indent=false&q.op=OR&q=*%3A*"
+    url = f"{solr_url}/select?{payload}&indent=false&q.op=OR&q=*%3A*&start={start}&row={row}"
 
     response = requests.get(url)
+    error = ""
 
-    return response.json()
+    response = response.json()
+
+    if(response["responseHeader"]["status"] >= 400):
+        error = "Error 404"
+        response = []
+    else:
+        response = response["response"]["docs"]
+
+    return (response, error)
 
 
-print(query_solr("hello", 5, "text", ["goose.com", "gdd.com", "jd.com"]))
+#print(query_solr("hello", 5, "text", ["www.gose.com", "gdd.com", "jd.com"]))
+
