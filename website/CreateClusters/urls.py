bd113d32408f7514c8046cc8da18f70081c4b429
MaishaAmin <maisha.amin@northsouth.edu>
2021-11-25 13:43:11 +0600

    UI changes

--- website/CreateClusters/urls.py
+++ website/CreateClusters/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-<<<<<<< HEAD
+from . import views
+from django.urls import path,include
 
-from . import views
-from django.urls import path,include
-=======
-from . import views
-from django.urls import path,include
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
->>>>>>> Maisha
 urlpatterns = [
 
     path('', views.index),
 
 ]
-<<<<<<< HEAD
-=======
-# urlpatterns += staticfiles_urlpatterns()
 
->>>>>>> Maisha
+
+
