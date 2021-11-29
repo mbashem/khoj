f60b1b5efb8ddfc601f1d33f105d924953b9965a
Farhan Ishraq <omifarhan@gmail.com>
2021-11-29 17:24:12 +0600

    spider connect

--- website/CreateClusters/views.py
+++ website/CreateClusters/views.py
@@ -3,7 +3,12 @@ import os
 from django.shortcuts import render, redirect
 from django.db import connection, connections
 import psycopg2
+
 import spiders
+
+import scrapy
+
+from scrapy.crawler import CrawlerProcess
 # Create your views here.
 
 from django.http import  HttpResponse
@@ -76,7 +81,10 @@ def StoreData(request):
         cursor.execute("""INSERT INTO "Cluster_Strategy"("Cluster_ID", "Strategy")
         VALUES ((select "Cluster_ID" from "Clusters" where "Cluster_Name"=%s), 'all text')""", [ClusterName])
 
+
     spiders.begin_crawl(URLS = URLS,height = Depth)
+
+
 
     return render(request, 'CreateClusters/ClusterIndex.html', {'msg' : 'cluster created successfully. System will let you know when it is ready to search'})
 
