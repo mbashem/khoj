20c715213da446be66e936f6ef87c5fb2f79f3a4
Farhan Ishraq <omifarhan@gmail.com>
2021-11-30 10:14:38 +0600

    change in spider

--- website/CreateClusters/spiders.py
+++ website/CreateClusters/spiders.py
@@ -10,7 +10,7 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerProcess
 from scrapy.crawler import CrawlerRunner
 
-
+#This is the crawler class for collecting nonhtml data
 class nonhtml_spider(scrapy.Spider):
 
     name = "nonhtml_spider"
@@ -19,38 +19,34 @@ class nonhtml_spider(scrapy.Spider):
     #SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
     #SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 
+    #This can be ignored
     start_urls = [
             'https://quotes.toscrape.com/page/1/'
     ]
     
-    
+    #This is the function that starts the crawling process
     def start_requests(self):
         self.urllist = []
         for link in self.urls:
             self.urllist.append(scrapy.Request(link,callback=self.parse, cb_kwargs=dict(cnt = 1,root_url = link)))
         return self.urllist
-            
+    #This is the callback function used to process the response
     def parse(self,response,cnt,root_url):
-
         
         yield {'url' : response.url,'depth' : cnt}
-        
 
         for txt in response.css("::text"):
-            var = txt.get().strip()
-            if len(var) != 0:
-                print(var)
-                print(cnt)
-                print(root_url)
-                print(response.url)
 
+            var = txt.get().strip()
+
+            if len(var) != 0:
+                #If the text is not empty we insert it into solr
                 indexer.insert.insert_into_solr_text( var, cnt, root_url, response.url, "nonhtml")
                 yield {'text' : var}
-         
-
-
+        #Checking is max depth is reached
         if(cnt<self.depth):
 
+            #Iterating through every link
              for nextpage in response.css('a::attr(href)'):
                  nextpage = nextpage.get()
                  if nextpage is not None:
@@ -58,12 +54,10 @@ class nonhtml_spider(scrapy.Spider):
                      yield scrapy.Request(response.urljoin(nextpage),callback=self.parse, cb_kwargs=dict(cnt = cnt+1,root_url = root_url))  
                     
 
-
+#The urls and depth from the create cluster form is passed to this function to begin the crawlng process
 def begin_crawl(URLS,height):
     process = CrawlerRunner(settings={
-    "FEEDS": {
-        "scrapped.json": {"format": "json"},
-    },
+    #"FEEDS": {"scrapped.json": {"format": "json"},},
     "DEPTH_PRIORITY" : 1,
     "SCHEDULER_DISK_QUEUE" : 'scrapy.squeues.PickleFifoDiskQueue',
     'SCHEDULER_MEMORY_QUEUE' : 'scrapy.squeues.FifoMemoryQueue',
@@ -72,7 +66,7 @@ def begin_crawl(URLS,height):
     p = process.crawl(nonhtml_spider,urls = URLS,depth = height)
     p.addBoth(lambda _: reactor.stop())
     #process.start()
-    reactor.run()
+    reactor.run(installSignalHandlers=False)
 
 
 
