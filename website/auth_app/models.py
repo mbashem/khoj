985a30dc159b4ba8b5dcb254c6e7d7bbf9febd0a
MaishaAmin <maisha.amin@northsouth.edu>
2021-12-06 12:58:23 +0600

    ui changes

--- website/auth_app/models.py
+++ website/auth_app/models.py
@@ -11,8 +11,6 @@ from django.db import models
 from django.db import models
 
 
-
-
 class AccountEmailaddress(models.Model):
     id = models.BigAutoField(primary_key=True)
     email = models.CharField(unique=True, max_length=254)
@@ -71,14 +69,13 @@ class AuthUser(models.Model):
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
     is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150, primary_key=True)
+    username = models.CharField(unique=True, max_length=150)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.CharField(max_length=254)
     is_staff = models.BooleanField()
     is_active = models.BooleanField()
     date_joined = models.DateTimeField()
-
 
     class Meta:
         managed = False
