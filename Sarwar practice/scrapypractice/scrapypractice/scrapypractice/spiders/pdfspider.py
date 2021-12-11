c8da877ababb8b9eb2ef867a26652e0e305fddd3
Sarwar Khalid <56037779+Nekrodeath@users.noreply.github.com>
2021-12-12 01:45:03 +0600

    Pdf spider

--- Sarwar practice/scrapypractice/scrapypractice/scrapypractice/spiders/pdfspider.py
+++ Sarwar practice/scrapypractice/scrapypractice/scrapypractice/spiders/pdfspider.py
@@ -1,62 +1,82 @@
+from crochet import setup,run_in_reactor
 import scrapy
+from twisted.internet import reactor
 from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
+import fitz
+import urllib.request
+import requests
+from io import BytesIO
+import PyPDF2
 
+def ispdf(str):
+     str  = str.split('.')
+     if(len(str)!=0 and str[-1]=='pdf'):
+         return True
+     else:
+         return False
+     
 
-class testspider(scrapy.Spider):
+class pdf_spider(scrapy.Spider):
 
-    name = "testspider"
+    name = "pdf_spider"
    
-    DEPTH_PRIORITY = 1
-    SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
-    SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
-
+    
     start_urls = [
             'https://quotes.toscrape.com/page/1/'
     ]
     
-
+   
     def start_requests(self):
-        return [scrapy.Request('http://localhost/Crawl_check/p1.html',callback=self.parse, cb_kwargs=dict(cnt = 1))]
-
-         
-
-    def parse(self,response,cnt):
-
+        self.urllist = []
+        for link in self.urls:
+            self.urllist.append(scrapy.Request(link,callback=self.parse, cb_kwargs=dict(cnt = 1,root_url = link)))
+        return self.urllist
+    
+    def parse(self,response,cnt,root_url):
         
         yield {'url' : response.url,'depth' : cnt}
-        print('THE CURRENT URL IS '+response.url)
+        if(ispdf(response.url)):
+            yield {'pdf':response.url,'depth':cnt}
 
-        for txt in response.css("::text"):
-            var = txt.get().strip()
-            if len(var) != 0:
-                yield {'text' : var}
-         
-
-
-        if(cnt<10):
+        if(cnt<self.depth):
 
              for nextpage in response.css('a::attr(href)'):
                  nextpage = nextpage.get()
-                 if nextpage is not None:
-                     print('THE URL IS '+response.urljoin(nextpage))
-                     yield scrapy.Request(response.urljoin(nextpage),callback=self.parse, cb_kwargs=dict(cnt = cnt+1))  
-                     
-
-
-process = CrawlerProcess(settings={
-    "FEEDS": {
-        "scrapped.json": {"format": "json"},
-    },
-})
-
-
-process.crawl(testspider)
-print("Starting crawl")
-process.start()
-print("Crawl ended")
+                 if(nextpage is not None):
+                    yield scrapy.Request(response.urljoin(nextpage),callback=self.parse, cb_kwargs=dict(cnt = cnt+1,root_url = root_url))  
                     
 
 
-            
+
+def begin_crawl(URLS,height):
+    process = CrawlerRunner(settings={
+    "FEEDS": {"scrapped.json": {"format": "json"},},
+    "DEPTH_PRIORITY" : 1,
+    "SCHEDULER_DISK_QUEUE" : 'scrapy.squeues.PickleFifoDiskQueue',
+    'SCHEDULER_MEMORY_QUEUE' : 'scrapy.squeues.FifoMemoryQueue',
+
+    })
+    p = process.crawl(pdf_spider,urls = URLS,depth = height)
+    p.addCallback(lambda _:reactor.stop())
+    reactor.run()
+
+data = requests.get("https://papers.gceguide.com/A%20Levels/Mathematics%20(9709)/2021/9709_s21_ms_13.pdf")
+
+data = data.content
+
+data = BytesIO(data)
+
+pdf_file = PyPDF2.PdfFileReader(data)
 
 
+for i in range(pdf_file.getNumPages()):
+    print(pdf_file.getPage(i).extractText())
+
+#begin_crawl(URLS = ['http://www.northsouth.edu/nsu-announcements/'],height = 2)
+print("DONE")
+
+#ispdf('http://www.northsouth.edu/nsu-announcements.pdf')
+
+
+                    
