f59d77d79eac0aa6c30204a593be9609c484b7c1
Farhan Ishraq <omifarhan@gmail.com>
2021-12-13 23:55:13 +0600

    api practice continues

--- practice-omi/api_practice/project/urls.py
+++ practice-omi/api_practice/project/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('stuinfo/<int:pk>', views.student_detail),
     path('stuinfo/', views.student_list),
+    path('stucreate/', views.student_create),
+    path('stuapi/', views.student_api),
 ]
--- practice-omi/api_practice/snippets/views.py
+++ practice-omi/api_practice/snippets/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+import io
 
+from django.shortcuts import render
+from rest_framework.parsers import JSONParser
 from .models import Student
 from .serializers import StudentSerializer
+
+import io
 
 from rest_framework.renderers import JSONRenderer
 
 from django.http import HttpResponse, JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def student_detail(request, pk):
@@ -25,3 +31,94 @@ def student_list(request):
 
     # return HttpResponse(json_data, content_type='application/json')
     return JsonResponse(serializer.data, safe=False)
+
+@csrf_exempt
+def student_create(request):
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+
+        if serializer.is_valid():
+            serializer.save()
+            msg = {'msg' : 'Data Inserted'}
+            json_data = JSONRenderer().render(msg)
+            return HttpResponse(json_data, content_type='application/json')
+
+    json_data = JSONRenderer().render(serializer.errors)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+
+@csrf_exempt
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body;
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        id = python_data.get('id', None)
+
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+
+        if serializer.is_valid():
+            serializer.save()
+            msg = {'msg': 'Data Inserted'}
+            json_data = JSONRenderer().render(msg)
+            return HttpResponse(json_data, content_type='application/json')
+
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        id = python_data.get('id')
+
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=python_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            msg = {'msg': 'data updated'}
+            json_data = JSONRenderer().render(msg)
+            return HttpResponse(json_data, content_type='application/json')
+
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        id = python_data.get('id')
+
+        stu = Student.objects.get(id=id)
+        stu.delete()
+
+        msg = {'msg' : 'data deleted'}
+
+        json_data = JSONRenderer().render(msg)
+
+        return HttpResponse(json_data, content_type='application/json')
+
+
