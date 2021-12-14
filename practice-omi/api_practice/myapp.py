8aede02ea8e21473b272ec1486ba27a7d6130fdd
Farhan Ishraq <omifarhan@gmail.com>
2021-12-14 20:55:46 +0600

    api practice

--- practice-omi/api_practice/myapp.py
+++ practice-omi/api_practice/myapp.py
@@ -2,13 +2,13 @@ import json
 
 import requests
 
-# URL = "http://127.0.0.1:8000/stuinfo/"
-#
-# r = requests.get(url=URL)
-#
-# data = r.json()
-#
-# print(data)
+URL = "http://127.0.0.1:8000/stuinfo/"
+
+r = requests.get(url=URL)
+
+data = r.json()
+
+print(data)
 
 
 # URL = "http://127.0.0.1:8000/stucreate/"
@@ -26,69 +26,69 @@ import requests
 #
 # print(data)
 
-
-URL = "http://127.0.0.1:8000/stuapi/"
-
-def get_data(id=None):
-    data = {}
-    if id is not None:
-        data = {'id' : id}
-
-    json_data = json.dumps(data)
-    r = requests.get(url=URL, data=json_data)
-    data = r.json()
-    print(data)
-
-
-# get_data()
-
-def post_data():
-    data = {
-        'name' : 'Omi Farhan',
-        'roll' : 200,
-        'city' : 'Cox Bazar'
-    }
-
-    json_data = json.dumps(data)
-
-    r = requests.post(url=URL, data = json_data)
-
-    data = r.json()
-
-    print(data)
-
-# post_data()
-
-
-def update_data():
-    data = {
-        'id' : 4,
-        'name' : 'Shakib AL Hasan',
-        'city' : 'Cox Bazar'
-    }
-
-    json_data = json.dumps(data)
-
-    r = requests.put(url=URL, data = json_data)
-
-    data = r.json()
-
-    print(data)
-
-# update_data()
-
-def delete_data():
-    data = {
-        'id' : 4
-    }
-
-    json_data = json.dumps(data)
-
-    r = requests.delete(url=URL, data = json_data)
-
-    data = r.json()
-
-    print(data)
-
-
-delete_data()
+#
+# URL = "http://127.0.0.1:8000/stuapi/"
+#
+# def get_data(id=None):
+#     data = {}
+#     if id is not None:
+#         data = {'id' : id}
+#
+#     json_data = json.dumps(data)
+#     r = requests.get(url=URL, data=json_data)
+#     data = r.json()
+#     print(data)
+#
+#
+# # get_data()
+#
+# def post_data():
+#     data = {
+#         'name' : 'Omi Farhan',
+#         'roll' : 200,
+#         'city' : 'Cox Bazar'
+#     }
+#
+#     json_data = json.dumps(data)
+#
+#     r = requests.post(url=URL, data = json_data)
+#
+#     data = r.json()
+#
+#     print(data)
+#
+# # post_data()
+#
+#
+# def update_data():
+#     data = {
+#         'id' : 4,
+#         'name' : 'Shakib AL Hasan',
+#         'city' : 'Cox Bazar'
+#     }
+#
+#     json_data = json.dumps(data)
+#
+#     r = requests.put(url=URL, data = json_data)
+#
+#     data = r.json()
+#
+#     print(data)
+#
+# # update_data()
+#
+# def delete_data():
+#     data = {
+#         'id' : 4
+#     }
+#
+#     json_data = json.dumps(data)
+#
+#     r = requests.delete(url=URL, data = json_data)
+#
+#     data = r.json()
+#
+#     print(data)
+#
+#
+# delete_data()
